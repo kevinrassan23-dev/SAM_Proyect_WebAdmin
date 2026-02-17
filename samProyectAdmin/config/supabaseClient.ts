87914ddf431f8d818811aee55787b918f665be3e
Palomo2001 <pablovegalamo@gmail.com>
2026-02-17 20:32:35 +0000

    les paso algo

--- samProyectAdmin/config/supabaseClient.ts
+++ samProyectAdmin/config/supabaseClient.ts
@@ -4,11 +4,4 @@
   EJEMPLO:*/
 
 // config/supabaseClient.ts
-/*import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'https://rokzxjuxifpxsytmqmvm.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_secret_p_LuqdmtT5bCQWyTUHTbRA_08zPQfn9';
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);*/
-
-//Descomenten esto por que por alguna razon github no me deja subirlo
