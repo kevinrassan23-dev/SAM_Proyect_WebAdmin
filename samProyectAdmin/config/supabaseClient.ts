829691c8378899adf6066b805522a3dbda7dd349
Palomo2001 <pablovegalamo@gmail.com>
2026-02-17 20:31:09 +0000

    descomentar

--- samProyectAdmin/config/supabaseClient.ts
+++ samProyectAdmin/config/supabaseClient.ts
@@ -7,6 +7,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const SUPABASE_URL = 'https://rokzxjuxifpxsytmqmvm.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_secret_p_LuqdmtT5bCQWyTUHTbRA_08zPQfn9';
+//const SUPABASE_ANON_KEY = 'sb_secret_p_LuqdmtT5bCQWyTUHTbRA_08zPQfn9';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
